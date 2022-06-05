735568ce73a817ad97b7b0b07e1a741b90874ce2
Stepan <420777122775s@gmail.com>
2022-06-05 20:09:51 +0500

    Start

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("1-Ввод А, 2-Ввод В, 3-Сложение, 4-Вычитание, 5-Умножение, 6-Деление");
+            int c = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
